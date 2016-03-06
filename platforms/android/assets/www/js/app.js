377ccce9ea204a9349998ff7857375bde34718a8
hidepy <hidork0222@yahoo.co.jp>
2016-03-06 17:25:45 +0900

    2016/03/06 アイコン・スプラッシュの設定書き換えたconfig作成

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,6 +1,6 @@
 (function(){
     'use strict';
-    var module = angular.module('app', ['onsen','checklist-model']);
+    var module = angular.module('CrooooberBrowserApp', ['onsen','checklist-model']);
     //var module = angular.module('app', ['onsen','checklist-model','ngAnimate']);
 
     //ヘッダ一覧画面のコントローラ
@@ -137,12 +137,16 @@
     });
 
     //詳細条件指定検索ページのコントローラ
-    module.controller('SearchDetailConditionController', function($scope) {
+    module.controller('SearchDetailConditionController', function($scope, CategoryMaster) {
 
         console.log("in SearchDetailConditionController");
 
         //車検索の場合にtrueとなる
         $scope.is_car_serach = false;
+        //カテゴリ情報をコピー
+        //1要素は, key=licatxxxx, value:{value: licatxxxx, name: カテゴリ名称, header: あるときは}
+        $scope.b_categories = CategoryMaster.b_categories_hash;
+        $scope.c_categories = CategoryMaster.c_categories_hash;
 
         //バイク/車検索チェック切り替え時発生のイベント
         $scope.bikeAndCarCheckChange = function(){
@@ -156,18 +160,6 @@
             //検索条件を組み立て＆保存
             storageManager.setSearchCondition(create_search_condition_params());
         };
-
-        //検索条件クリア
-        function search_condition_clear(){
-            console.log("search condition clear");
-
-            $("#setting_search_condition_keyword").val("");
-            $("input[name=search_condition_sort]").val(["1"]);
-            $("#search_condition_bunrui_list_wrapper > li").removeClass("bunrui_list_selected");
-            $("#search_condition_bunrui_list_wrapper_car > li").removeClass("bunrui_list_selected");
-            $("#setting_search_condition_kakaku_low").val("");
-            $("#setting_search_condition_kakaku_high").val("");
-        }
 
         //詳細検索条件作成
         function create_search_condition_params(){
@@ -193,13 +185,19 @@
                 return "";
             })();   
 
+            var connector = $('input[name=select_and_or]:checked').val();
+            if((connector != "and") && (connector != "or")){ //andでもorでもないならデフォルト値をセット
+                connector = "and";
+            }
+
             var detail_param = {
                 word: $("#setting_search_condition_keyword").val(),
-                connector: "and",
+                connector: connector,
                 bunrui: bunrui_cd,
                 kakaku_low: $("#setting_search_condition_kakaku_low").val(),
                 kakaku_high: $("#setting_search_condition_kakaku_high").val(),
-                sort: sort_kbn || "1"
+                sort: sort_kbn || "1",
+                search_type: $('input[name=select_bike_or_car]:checked').val() || "bike"
             };
 
             return detail_param;
@@ -301,14 +299,27 @@
         };
     });
 
-
     //保存した検索条件のコントローラ
-    module.controller('ViewSavedSearchConditionController', function($scope) {
+    module.controller('ViewSavedSearchConditionController', function($scope, CategoryMaster) {
 
         console.log("in ViewSavedSearchConditionController");
 
         //保存した検索条件情報のロード
-        $scope.items = storageManager.getAllSearchConditionItemsAsArr();
+        //$scope.items = storageManager.getAllSearchConditionItemsAsArr();
+
+        $scope.items = (function(){
+            var saved_cond = storageManager.getSearchCondition();
+
+            for(var key in saved_cond){
+                var bunrui_cd = save_cond[key].bunrui; //保存した検索条件 の bundui
+                var master_type = save_cond[key].search_type; //bk or cr
+
+                //マスタから名称をひいてくる
+                saved_cond[key].bunrui_name = (master_type == "bike") ? CategoryMaster.b_categories_hash[bunrui_cd] : CategoryMaster.c_categories_hash[bunrui_cd];
+            }
+
+            return saved_cond;
+        })();
 
         //削除するデータリスト
         $scope.del = {
@@ -332,7 +343,6 @@
             }
             else{
                 //削除　ボタン表示中なら
-
                 var target_idx = $scope.del.items.indexOf($scope.items[index].word);
 
                 //既に登録されているか
@@ -375,6 +385,82 @@
         $scope.uncheckAll = function() {
             $scope.del.items = [];
         };
+    });
+
+    /* カテゴリマスタ */
+    module.factory("CategoryMaster", function(){
+        var data = {};
+
+        data.b_categories_hash = {  //直前にヘッダを持っている場合、ヘッダ情報を吐く
+            //licat9010:{value: "licat9010", name: "ts", header: ""},
+            none: {value: "", name:"(指定なし)"},
+            licat9010:{value: "licat9010", name: "マフラー", header: "<ons-list-header>バイクパーツ</ons-list-header>"},
+            licat9015:{value: "licat9015", name: "外装"},
+            licat9020:{value: "licat9020", name: "ハンドル・ハンドル廻り"},
+            licat9025:{value: "licat9025", name: "メーター"},
+            licat9030:{value: "licat9030", name: "ステップ・スタンド"},
+            licat9035:{value: "licat9035", name: "足廻り"},
+            licat9040:{value: "licat9040", name: "駆動系"},
+            licat9045:{value: "licat9045", name: "ホイール・タイヤ"},
+            licat9050:{value: "licat9050", name: "ブレーキ"},
+            licat9055:{value: "licat9055", name: "電装系"},
+            licat9060:{value: "licat9060", name: "冷却系"},
+            licat9065:{value: "licat9065", name: "吸気・燃料系"},
+            licat9070:{value: "licat9070", name: "エンジン・フレーム"},
+            licat9075:{value: "licat9075", name: "その他（バイクパーツ）"},
+            licat9210:{value: "licat9210", name: "ヘルメット", header: "<ons-list-header>バイク用品</ons-list-header>"},
+            licat9220:{value: "licat9220", name: "ウエア"},
+            licat9230:{value: "licat9230", name: "ブーツ・シューズ"},
+            licat9240:{value: "licat9240", name: "グローブ・ゴーグル"},
+            licat9250:{value: "licat9250", name: "プロテクター"},
+            licat9260:{value: "licat9260", name: "ツーリング用品"},
+            licat9270:{value: "licat9270", name: "メンテナンス"},
+            licat9280:{value: "licat9280", name: "ケミカル・オイル"},
+            licat9299:{value: "licat9299", name: "その他（バイク用品）"}
+        };
+        data.c_categories_hash = {
+            none: {value: "", name:"(指定なし)"},
+            licat1001:{value: "licat1001", name: "タイヤ", header: "<ons-list-header>タイヤ・ホイール</ons-list-header>"},
+            licat1005:{value: "licat1005", name: "スタッドレスタイヤ"},
+            licat2001:{value: "licat2001", name: "アルミホイール"},
+            licat2002:{value: "licat2002", name: "スチールホイール"},
+            licat2501:{value: "licat2501", name: "タイヤホイールセット"},
+            licat2503:{value: "licat2503", name: "スタッドレスタイヤホイール"},
+            licat2605:{value: "licat2605", name: "タイヤホイール関連"},
+            licat3001:{value: "licat3001", name: "ヘッドユニット", header: "<ons-list-header>カーAV</ons-list-header>"},
+            licat3002:{value: "licat3002", name: "スピーカー"},
+            licat3003:{value: "licat3003", name: "アンプ"},
+            licat4001:{value: "licat4001", name: "カーナビ(地デジ）"},
+            licat4005:{value: "licat4005", name: "カーナビ(非地デジ）"},
+            licat4010:{value: "licat4010", name: "オンダッシュモニター"},
+            licat4015:{value: "licat4015", name: "インダッシュモニター"},
+            licat4020:{value: "licat4020", name: "モニター・地デジ"},
+            licat4025:{value: "licat4025", name: "DVDプレーヤー"},
+            licat4030:{value: "licat4030", name: "カーAVアクセサリー"},
+            licat4035:{value: "licat4035", name: "カーナビ(単体・その他)"},
+            licat4040:{value: "licat4040", name: "ETC"},
+            licat5001:{value: "licat5001", name: "足まわり", header: "<ons-list-header>カスタム・チューニング</ons-list-header>"},
+            licat5010:{value: "licat5010", name: "吸気・排気系"},
+            licat5015:{value: "licat5015", name: "電装系"},
+            licat5020:{value: "licat5020", name: "バルブ・HID"},
+            licat5025:{value: "licat5025", name: "補強パーツ"},
+            licat5030:{value: "licat5030", name: "メーター系"},
+            licat5035:{value: "licat5035", name: "過給機系"},
+            licat5040:{value: "licat5040", name: "駆動系"},
+            licat5045:{value: "licat5045", name: "冷却系"},
+            licat5050:{value: "licat5050", name: "ブレーキ系"},
+            licat5055:{value: "licat5055", name: "ボディパーツ"},
+            licat5060:{value: "licat5060", name: "インテリア"},
+            licat5065:{value: "licat5065", name: "シート"},
+            licat5070:{value: "licat5070", name: "キャリア"},
+            licat5099:{value: "licat5099", name: "その他(カスタム・チューニング)"},
+            licat7001:{value: "licat7001", name: "ケミカル用品", header: "<ons-list-header>カー用品</ons-list-header>"},
+            licat7010:{value: "licat7010", name: "メンテナンス"},
+            licat7020:{value: "licat7020", name: "アクセサリー"},
+            licat7099:{value: "licat7099", name: "その他(カー用品)"}
+        };
+
+        return data;
     });
 
 })();
